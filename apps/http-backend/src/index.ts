5fc4177f9c349c33ca80a4cc3ac22c97cc13fb2e
Dipanshu Zalke <dipanshuzalke@gmail.com>
2025-05-13 02:47:49 +0530

    done for the day

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,34 +1,65 @@
 import express from "express";
-import { JWT_SECRET } from "./config";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import {
+  CreateRoomSchema,
+  CreateUserSchema,
+  SigninSchema,
+} from "@repo/common/types";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
 
-})
+  res.json({
+    userId: "123",
+  });
+});
 
 app.post("/signin", (req, res) => {
-
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET)
-
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
     res.json({
-        token
-    })
-})
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    token,
+  });
+});
 
 app.post("/room", middleware, (req, res) => {
-
-
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
     res.json({
-        roomId: 123
-    })
-})
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
+  res.json({
+    roomId: 123,
+  });
+});
 
 app.listen(3000, () => {
-    console.log("Server listening on port 3000")
+  console.log("Server listening on port 3000");
 });
