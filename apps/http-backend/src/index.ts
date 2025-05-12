b41ee6b0cac9a4550b6d6b6c9ddd2d1555d6d8bd
Dipanshu Zalke <dipanshuzalke@gmail.com>
2025-05-13 01:05:47 +0530

    #

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,33 @@
 import express from "express";
+import { JWT_SECRET } from "./config";
+import jwt from "jsonwebtoken"
+import { middleware } from "./middleware";
 
 const app = express();
+
+app.post("/signup", (req, res) => {
+
+})
+
+app.post("/signin", (req, res) => {
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+
+    res.json({
+        token
+    })
+})
+
+app.post("/room", middleware, (req, res) => {
+
+
+    res.json({
+        roomId: 123
+    })
+})
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000")
